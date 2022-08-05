43ac9a52b26a7baef6810b34a87070920071c0c0
SaltSith <SaltSith66@gmail.com>
2022-08-05 21:52:36 +0200

    new empty source files and project settings

---
+++ Arduino - eeprom memory manager/Arduino - eeprom memory manager.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino___eeprom_memory_manager.ino
+ Created:	8/5/2022 9:49:53 PM
+ Author:	marcin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
