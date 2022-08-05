3575d0d5ee0d2cd81c5cd277cdd658658fcbe38f
SaltSith <SaltSith66@gmail.com>
2022-08-05 22:15:35 +0200

    init function verify init value and erase eeprom if necessary, compute address for each defined data in config

--- Arduino - eeprom memory manager/data_storage.cpp
+++ Arduino - eeprom memory manager/data_storage.cpp
@@ -22,6 +22,29 @@ void data_storage::mark_as_free(const data_type_t id)
 
 void data_storage::init(void)
 {
+	this->size = EEPROM.length();
+	this->marker = 0;
+
+	uint8_t read_init_value = EEPROM.read(BASE_ADDRESS);
+
+	if (read_init_value != data_storage_init_value) {
+		Serial.println("Erasing eeprom");
+		for (uint16_t i = 0; i < this->size; i++) {
+			EEPROM.write(i, 0);
+		}
+		EEPROM.write(BASE_ADDRESS, data_storage_init_value);
+	}
+
+	uint16_t data_storage_base_address = sizeof(data_storage_init_value) + sizeof(this->marker);
+
+	for (uint8_t id = DS_1; id < DS_LAST; id++) {
+		data_storage_config[id].addr = data_storage_base_address;
+		data_storage_base_address += data_storage_config[id].size;
+
+		Serial.print(data_storage_config[id].id); Serial.print(" ");
+		Serial.print(data_storage_config[id].addr); Serial.print(" ");
+		Serial.println(data_storage_config[id].size);
+	}
 }
 
 int data_storage::set(const data_type_t id, const uint8_t * source)
