9f97563594512edf2a2e43e47bd2cfbc5a8e5521
SaltSith <SaltSith66@gmail.com>
2022-08-05 21:57:08 +0200

    define enumeration of data type, and the struture with data info

--- Arduino - eeprom memory manager/data_storage.h
+++ Arduino - eeprom memory manager/data_storage.h
@@ -1,2 +1,23 @@
 #pragma once
 
+#include <stdint.h>
+
+/*
+ * The enumeration of data types kept in eeprom.
+ */
+typedef enum {
+	DS_1 = 0,
+	DS_2,
+	DS_3,
+	DS_LAST
+} data_type_t;
+
+/*
+ * The structure defines data info.
+ */
+typedef struct {
+	const data_type_t id;		/// Data id
+	uint16_t addr;				/// Address in eeprom memory
+	uint16_t size;				/// Sizeof data in bytes
+} data_info_t;
+
